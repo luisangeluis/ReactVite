d0266c2d7ed24e7e9cd17b14ddfa8448e13acfdc
luisangeluis <luisangeluis@gmail.com>
2022-06-16 19:25:39 -0500

    hooks form

---
+++ hook-form/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import {useForm} from 'react-hook-form';
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      
+    </div>
+  )
+}
+
+export default App
