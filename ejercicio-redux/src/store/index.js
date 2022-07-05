b3a8b71addb03561b7c745069a79ee0618008545
luisangeluis <luisangeluis@gmail.com>
2022-07-04 19:08:08 -0500

    Ejercicio con redux

--- ejercicio-redux/src/store/index.js
+++ ejercicio-redux/src/store/index.js
@@ -1,7 +1,8 @@
-import {configureStore} from '@reduxjs/toolkit';
-import countries from './slices/countries.slice'
+import { configureStore } from "@reduxjs/toolkit";
+import countries from "./slices/countries.slice";
+
 export default configureStore({
-    reducer:{
-        countries
-    }
-})
+  reducer: {
+    countries,
+  },
+});
